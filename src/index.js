a5dd160f3faabd4ddfadc3e473cf9f0fc258e7e2
Marty <maruri2424@gmail.com>
2021-12-19 00:45:59 -0800

    made sidebar with collapsible section and wrote todo factory function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('test');
+import toDo from "./todo"
+import './style.css';
+
+const collapse = document.querySelectorAll('.collapsible');
+
+collapse.forEach(e => e.addEventListener('click', controlCollapse));
+
+function controlCollapse () {
+  this.classList.toggle('active');
+  const content = this.nextElementSibling;
+  if (content.style.display === 'block') {
+    content.style.display = 'none';
+  } else {
+    content.style.display = 'block';
+  }
+}
---
+++ src/todo.js
@@ -0,0 +1,22 @@
+const toDo = (title, notes, date, priority) => {
+  const getTitle = () => title;
+  const updateTitle = (newTitle) => {
+    title = newTitle;
+  }
+  const getNotes = () => notes;
+  const updateNotes = (newNotes) => {
+    notes = newNotes;
+  }
+  const getDate = () => date;
+  const updateDate = (newDate) => {
+    date = newDate;
+  }
+  const getPriority = () => priority;
+  const updatePriority = (newPriority) => {
+    priority = newPriority;
+  }
+  return {getTitle, updateTitle, getNotes, updateNotes, 
+    getDate, updateDate, getPriority, updatePriority};
+}
+
+export default toDo;
